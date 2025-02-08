3e1dda41502e39a9cbe7f2ef787f3c391949ae9a
Nayenuen <leyangm88@gmail.com>
2025-02-08 00:13:02 -0700

    Update abre.php

--- abre.php
+++ abre.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost","root","","cines");
+$conexion = new mysqli("sql307.infinityfree.com","if0_38264800","Rz86gb6BUJQ","if0_38264800_cines");
 if ($conexion) {
     echo "la gestion fue exitosa!!";
 }else{
